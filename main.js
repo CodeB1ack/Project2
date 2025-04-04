d9c77a5b2ce19e838e7684a888e541b76363b6bc
CodeB1ack <patrickcledford@gmail.com>
2025-04-03 22:17:14 -0400

    added js for search bar

---
+++ main.js
@@ -0,0 +1,21 @@
+const toggleSearch = () => {
+    const searchForm = document.querySelector('.search-form');
+    const searchButton = document.querySelector('.search-button');
+    const searchInput = document.querySelector('.search-input');
+
+    searchButton.addEventListener('click', () => {
+        searchForm.classList.toggle('active-search');
+    });
+    searchInput.addEventListener('keydown', (e) => {
+        
+        if (e.key === 'Enter') {
+            searchForm.classList.remove('active-search');
+            e.preventDefault();
+            searchInput.value = '';
+            searchForm classList.remove('active-search');
+        }
+    });
+
+};
+
+toggleSearch();
